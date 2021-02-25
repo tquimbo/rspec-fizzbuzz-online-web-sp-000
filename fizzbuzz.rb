0a0b6f7ff9558e831a2aae2446d70552d284e7a5
Tim Quimbo <tquimbo1@gmail.com>
2021-02-25 22:53:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,14 +3,18 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  elsif int % 5 == 0
-    "Buzz"
   end
 end
 
-def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-  elsif int % 5 == 0 # if the number int is divisible by 5
-    "FizzBuzz" # Go fizzbuzz
-  end
-end
+# def fizzbuzz(int)
+#   if int % 5 == 0 # if the number int is divisible by 5
+#     "Buzz" # Go buzz
+#   end
+# end
+# 
+# def fizzbuzz(int)
+#   if int % 3 == 0 # if the number int is divisible by 3
+#   elsif int % 5 == 0 # if the number int is divisible by 5
+#     "FizzBuzz" # Go fizzbuzz
+#   end
+# end
